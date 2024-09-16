2245f9ff62b96ec86f88da8c19690b5b6bdae740
ahmedmah010 <reahmedmah010@gmail.com>
2024-09-17 02:47:40 +0300

    Fresh Start
    A fresh start with the project structure (clean architecture).

---
+++ SocialMediaAPI.Infrastructure/Context/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SocialMediaAPI.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMediaAPI.Infrastructure.Context
+{
+    public class AppDbContext : IdentityDbContext<AppUser, IdentityRole, string>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { }
+    }
+}
---
+++ SocialMediaAPI.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SocialMediaAPI.Domain.Entities;
+using SocialMediaAPI.Infrastructure.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMediaAPI.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("Default")));
+            services.AddIdentityCore<AppUser>().AddEntityFrameworkStores<AppDbContext>();
+            return services;
+        }
+    }
+}
